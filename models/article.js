4798adcf26072a9d0c20c15dcb6b4e7ceb632594
tpsef <pseftelistheo@gmail.com>
2020-06-12 19:39:24 +0300

    Update article model with Joi user input validation function

--- models/article.js
+++ models/article.js
@@ -1,4 +1,6 @@
 const  mongooose = require('mongoose');
+const Joi = require('@hapi/joi');
+Joi.objectId = require('joi-objectid')(Joi);
 
 
 const article_shema = new mongooose.Schema({
@@ -13,7 +15,8 @@ const article_shema = new mongooose.Schema({
         minlength: 5
     },
     published_status: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     published_date:{
         type: Date
@@ -25,6 +28,17 @@ const article_shema = new mongooose.Schema({
 
 const Article = mongooose.model('Article', article_shema);
 
-// TODO: User input validation Joi
+function validateArticle(article) {
+    const schema = Joi.object({
+        title: Joi.string().min(3).max(250).required(),
+        summary: Joi.string().min(3),
+        published_status: Joi.boolean(),
+        published_date: Joi.date(),
+        author: Joi.array().items(Joi.objectId()),
+        category: Joi.array().items(Joi.objectId()),
+    }); 
+    return schema.validate(article);
+}
 
 module.exports.Article = Article;
+module.exports.validateArticle = validateArticle;
