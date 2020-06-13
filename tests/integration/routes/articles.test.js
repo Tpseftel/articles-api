85026f603a5a645272fe994b43b6b81dd4f923ca
tpsef <pseftelistheo@gmail.com>
2020-06-13 08:50:42 +0300

    Test POST method on articles endpoint

--- tests/integration/routes/articles.test.js
+++ tests/integration/routes/articles.test.js
@@ -2,46 +2,58 @@ const debug = require('debug')('app: tests/articles.test.js');
 const mongoose = require('mongoose');
 const request = require('supertest');
 const { Article } = require('../../../models/article');
+const { expectCt } = require('helmet');
 
 let server;
 describe('/api/articles', () => {
-    describe('Name of the group', () => {
-        beforeEach( () => {
-            server = require('../../../index');     
-        });
+    let article;
+    beforeEach( () => {
+        server = require('../../../index');     
 
-        afterEach( async () => {
-            await Article.deleteMany({});
-            await server.close();
-        });
-
-        describe('GET /', () => {
-
-            it('should return all articles ', async () => {
-                const article1 = {
-                    title:'Amazing article 1',
-                    summary: 'Amazing summary1',
-                    published_status: true,
-                    // FIXME: Use moment
-                    published_date: Date.now(),
-                };
-                const article2 = {
-                    title: 'Amazing article 2',
-                    summary: 'Amazing summary 2',
-                    published_status: false,
-                    published_date: Date.now(),
-                };
-
-                await Article.insertMany([article1, article2]);
-
-                const response = await request(server).get('/api/articles');
-                expect(response.status).toBe(200);
-                debug(response.body);
-                expect(response.body.length).toBeGreaterThan(1);
-            });
-            
-        });
-
+        article = {
+            title:'Amazing article 1',
+            summary: 'Amazing summary1',
+            published_status: true,
+            // FIXME: Use moment
+            published_date: Date.now(),
+        };
     });
-    
+
+    afterEach( async () => {
+        await Article.deleteMany({});
+        await server.close();
+    });
+   
+    describe('GET /', () => {
+        it('should return all articles ', async () => {
+            await Article.insertMany([article]);
+
+            const response = await request(server).get('/api/articles');
+            debug(response.body);
+            expect(response.status).toBe(200);
+            expect(response.body.length).toBeGreaterThan(0);
+        });
+        
+    });
+
+    describe('POST /', () => {
+        it('should return 400 status code if validation req.body fails', async () => {
+            article.title = 'a';
+            const response = await request(server)   
+                .post('/api/articles/')
+                .send(article);
+
+            expect(response.status).toBe(400);
+        });
+
+        it('should save the new article if user input is valid', async () => {
+            await request(server)   
+                .post('/api/articles/')
+                .send(article);
+
+            const saved_article =  await Article.findOne({title: article.title});    
+            expect(saved_article).toBeTruthy();                       
+        });
+        
+    });
 });
