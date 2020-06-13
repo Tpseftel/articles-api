76e7803e53b672aa0efde5fdf5ef19658e4c2323
tpsef <pseftelistheo@gmail.com>
2020-06-13 19:19:10 +0300

    Update articles router

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,10 @@
+const { Author } = require('../models/author');
+const { Category } = require('../models/category');
 const debug = require('debug')('app: articles router');
 const express = require('express');
 const { Article, validateArticle } = require('../models/article');
 const router = express.Router();
 const mongoose = require('mongoose');
-
 
 // Get all articles
 router.get('/', async (req, res) => {
@@ -19,14 +20,24 @@ router.post('/', async (req, res) => {
     const { error } = validateArticle(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
-    const article = new Article({
+    // Check if there is the given author
+    const author = await Author.findById(req.body.author);
+    if(!author) return res.status(404).send('Author not found');
+
+    // Check if there is the given category
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(404).send('Category not found');
+
+    let article = new Article({
         title: req.body.title,
         summary: req.body.summary,
         published_status: req.body.published_status,
         published_date: req.body.published_date,
+        author: req.body.author,
+        category: req.body.category
     });
-    const saved_article = await article.save();
 
+    let saved_article = await article.save();
     res.send(saved_article);
 });
 
@@ -38,12 +49,24 @@ router.put('/:id', async (req, res) => {
     }
 
     // Check if there is the requested article
-    let article = await Article.findById(req.params.id);
+    const article = await Article.findById(req.params.id);
     if (!article) return res.status(404).send('The article with the given ID was not found.');
 
     // Check if is valid input
     const {error} = validateArticle(req.body);
     if (error) return res.status(400).send(error.details[0].message);
+
+    if(req.body.author){
+        // Check if there is the given author
+        const author = await Author.findById(req.body.author);
+        if(!author) return res.status(404).send('Author not found');
+    }
+
+    if (req.body.category) {
+        // Check if there is the given category
+        const category = await Category.findById(req.body.category);
+        if(!category) return res.status(404).send('Category not found');
+    }
 
     const updated_article = await Article.findByIdAndUpdate(
         req.params.id,
