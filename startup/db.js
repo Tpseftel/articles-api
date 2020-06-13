874fe643a403e9418e8f9b586739b16ced560af7
tpsef <pseftelistheo@gmail.com>
2020-06-13 09:02:58 +0300

    Update db.js
    Check if test enviroment to se topology option

--- startup/db.js
+++ startup/db.js
@@ -3,10 +3,16 @@ const mongoose = require('mongoose');
 mongoose.set('debug', true);
 
 
+let topology = process.env.NODE_ENV == 'test' ? false : true;
+
 module.exports = function () {
-        mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true,  useUnifiedTopology: false })
-        .then(res => {
-            debug('Connecting to Mongo...\n');
+        mongoose.connect('mongodb://localhost:27017/blogDB',
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: topology 
+            }
+        ).then(res => {
+            debug('Connected to Mongo...\n');
         }).catch(error => {
             debug('Error: ' + error.name + ' ' + error.message);
         });
