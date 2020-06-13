b851550445a2faadf8361542d8f05aba0bb720d3
tpsef <pseftelistheo@gmail.com>
2020-06-14 02:10:44 +0300

    Update index.js
    Handle Errors
    - uncaughtException
    - unhandledRejection

--- index.js
+++ index.js
@@ -6,26 +6,31 @@ const express = require('express');
 const config = require('config');
 const app = express();
 
+// Handle Errors
+process.on('uncaughtException',(ex) => {
+    console.log(ex);
+    process.exit(1);
+});
+process.on('unhandledRejection',(ex) => {
+    console.log(ex);
+    process.exit(1);
+});
 
-app.use(helmet());
-app.use(express.json());
 
 debug(`App name: ${config.get('app.name')}`);
-
 if(app.get('env') === 'production') {
     if( !config.get('app.jwtPrivateKey') ) {
         throw new Error('jwtPrivateKey is not defined.');
     }
 }
 
-
+app.use(helmet());
+app.use(express.json());
 require('./startup/db')();
 require('./startup/routes')(app);
 app.use(handleErrors);
 
-
-
 const port = process.env.PORT || 3000;
-
 const server = app.listen(port, () => console.log(`Listening on port ${port}...`)); 
+
 module.exports = server;
